18196faa665d8d2524f9000d442682c6e452bbe3
noomdalv <vladlunasan@gmail.com>
2024-03-29 01:38:39 -0500

    qwerty

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './styles.css';
+import "./styles.css";
 
 const Footer = () => {
-	return (
-		<div className="footer">
-			<h5>Made by <span> Vlad Luna </span></h5>			
-		</div>
-	)
-}
+  return (
+    <div className="footer">
+      <h5>
+        Made by <span> Vlad Luna </span>
+      </h5>
+    </div>
+  );
+};
 
 export default Footer;
