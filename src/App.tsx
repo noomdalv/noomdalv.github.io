5073f6fb8d65aa207732b514e5976a5c74c0f523
noomdalv <vladlunasan@gmail.com>
2024-04-19 01:20:11 -0500

    new navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Projects from './components/Projects';
-import About from './components/About';
-import Footer from './components/Footer';
+import Landing from "./components/Landing";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 const App = () => {
-	return (
-		<div className="app">
-			<Nav />
-			<Landing />
-			<Projects />
-			<About />
-			<Footer />
-		</div>
-	)
-}
+  return (
+    <div className="app">
+      <Nav />
+      <Landing />
+      <Projects />
+      <About />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,41 @@
-import './styles.css';
+import { useEffect } from "react";
+import "./styles.css";
 
-const Nav = () => {
-	return (
-		<div className="nav">
-			<ul>
-				<li><a href="#root">Home</a></li>
-				<li><a href="#projects">Projects</a></li>
-				<li><a href="#about">About</a></li>
-			</ul>
-		</div>
-	)
+function Nav2() {
+  useEffect(() => {
+    // Get the navbar element
+    const navbar = document.querySelector<HTMLElement>(".navbar");
+    if (navbar) {
+      // Define the mousemove event handler
+      const handleMouseMove = (event: MouseEvent) => {
+        navbar.style.setProperty("--mouse-x", `${event.clientX}px`);
+        navbar.style.setProperty("--mouse-y", `${event.clientY}px`);
+      };
+
+      // Attach the event listener
+      navbar.addEventListener("mousemove", handleMouseMove);
+
+      // Cleanup function to remove the event listener when the component unmounts
+      return () => {
+        navbar.removeEventListener("mousemove", handleMouseMove);
+      };
+    }
+  }, []); // Empty dependency array to run the effect only once
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="z-10">
+          <a href="#root">Hello</a>
+        </li>
+        <li className="z-10">
+          <a href="#projects">Projects</a>
+        </li>
+        <li className="z-10">
+          <a href="#about">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav;
+export default Nav2;
